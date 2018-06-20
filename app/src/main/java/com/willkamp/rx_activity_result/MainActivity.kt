25d42d23bfeb623c8725ff13e9fdc644d4ea1ca3
William Kamp <manimaul@gmail.com>
2018-06-20 09:23:55 -0700

    initial commit

---
+++ app/src/main/java/com/willkamp/rx_activity_result/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.willkamp.rx_activity_result
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
---
+++ rxar/src/main/java/com/willkamp/rxar/RxActivityResult.kt
@@ -0,0 +1,87 @@
+package com.willkamp.rxar
+
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
+import java.security.SecureRandom
+import java.util.concurrent.ThreadLocalRandom
+
+private const val MIN_16BIT_INT = 0
+private const val MAX_16BIT_INT = 65535
+private fun rand16BitInt(): Int {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+    ThreadLocalRandom.current().nextInt(MIN_16BIT_INT, MAX_16BIT_INT + 1)
+  } else {
+    SecureRandom().nextInt(MAX_16BIT_INT - MIN_16BIT_INT + 1) + MIN_16BIT_INT
+  }
+}
+
+private val TAG = ActivityResult::class.java.simpleName
+private const val KEY_INTENT = "KEY_INTENT"
+private const val KEY_OPTIONS = "KEY_OPTIONS"
+private const val KEY_REQUEST_CODE = "KEY_REQUEST_CODE"
+
+object RxActivityResult {
+
+  @JvmOverloads
+  fun startActivityForResult(fragmentManager: FragmentManager,
+                             intent: Intent,
+                             options: Bundle? = null): Single<ActivityResult> {
+    return Single.defer {
+      val broker = ActivityResultBroker()
+      val args = Bundle()
+      args.putParcelable(KEY_INTENT, intent)
+      args.putInt(KEY_REQUEST_CODE, rand16BitInt())
+      if (options != null) {
+        args.putParcelable(KEY_OPTIONS, options)
+      }
+      broker.arguments = args
+
+      fragmentManager.beginTransaction()
+          ?.add(broker, TAG)
+          ?.commit()
+      broker.resultSingle
+    }.subscribeOn(AndroidSchedulers.mainThread())
+  }
+}
+
+internal class ActivityResultBroker : Fragment() {
+
+  private val resultSubject = PublishSubject.create<ActivityResult>()
+  val resultSingle: Single<ActivityResult>
+    get() = resultSubject.singleOrError()
+
+  private val requestCode: Int
+    get() = arguments?.getInt(KEY_REQUEST_CODE) ?: 0
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    retainInstance = true
+    arguments?.getParcelable<Intent>(KEY_INTENT)?.let { intent ->
+      arguments?.getBundle(KEY_OPTIONS)?.let { options ->
+        startActivityForResult(intent, requestCode, options)
+      } ?: startActivityForResult(intent, requestCode)
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    if (requestCode == this.requestCode) {
+      resultSubject.onNext(ActivityResult(requestCode, resultCode, data))
+      resultSubject.onComplete()
+      fragmentManager?.beginTransaction()
+          ?.remove(this)
+          ?.commit()
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    resultSubject.onComplete()
+  }
+}
